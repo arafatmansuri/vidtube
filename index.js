f149b93883ec2909e37ddbef77df1d2c241b2527
arafatmansuri <mansuriarafat302@gmail.com>
2025-01-25 14:45:34 +0530

    Added index.js file for to fetch data from cookies

---
+++ index.js
@@ -0,0 +1,30 @@
+async function fetchHomePage() {
+  let result;
+  try {
+    const response = await fetch(
+      "http://localhost:8000/api/v1/student/getuser",
+      {
+        method: "GET",
+        credentials: "include", // This ensures cookies are sent with the request
+      }
+    );
+
+    result = await response.json();
+    if (response.ok) {
+      console.log("Home Page Data:", result);
+      console.log("Home Page Data:", result.data);
+      return result.data;
+    } else {
+      console.error("Failed to load home page:", result.message);
+    }
+  } catch (error) {
+    console.error("Error fetching home page:", error);
+  }
+}
+
+async function displayData() {
+  const user = await fetchHomePage();
+  console.log(user.fullName);
+}
+
+displayData();
